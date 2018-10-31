f09a02927b65dbfe320a6a8edde92d9fb7e70452
ruanweiguang <rwgweiguang@163.com>
2018-10-31 19:20:22 +0800

    增加进程

--- bin/index.js
+++ bin/index.js
@@ -2,10 +2,12 @@ const path = require('path');
 const angelWebpack = require('../lib/angel-webpack');
 const AngelServer = require('../lib/server');
 const cluster = require('cluster');
+
 //使用mongodb
 require('../app/tool/mongodb.js');
 //记录系统运行日志
 require('../app/tool/system-log.js')
+
 //fork一个新的进程，用于启动webpack
 if(cluster.isMaster) {
   new angelWebpack({
